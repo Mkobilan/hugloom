71fc8dbd1f5567b5de54abbf2c79568b990588c2
Mkobilan <matthew.kobilan@gmail.com>
2025-11-26 17:34:38 +0600

    fixed background on threads

--- src/app/thread/[commentId]/page.tsx
+++ src/app/thread/[commentId]/page.tsx
@@ -69,58 +69,77 @@ export default function ThreadPage() {
         if (parent) {
             ancestors.unshift(parent);
             current = parent;
-            <div key={ancestor.id} className="relative">
-                <CommentItem
-                    comment={ancestor}
-                    currentUserId={currentUserId}
-                    onCommentUpdated={refreshComments}
-                    isThreadView={true}
-                />
-                {/* Connector line to next item */}
-                <div className="absolute left-[27px] top-[60px] bottom-[-16px] w-0.5 bg-border/50 -z-10" />
-            </div>
-                    ))
+        } else {
+            break;
         }
-                </div >
+    }
 
-            {/* Main Comment */ }
-            < div className = "relative" >
-                <div className="text-xl">
-                    <CommentItem
-                        comment={fullTargetComment}
-                        currentUserId={currentUserId}
-                        onCommentUpdated={refreshComments}
-                        isThreadView={true}
-                        isHero={true} // New prop to style the main comment larger
+    return (
+        <div className="max-w-2xl mx-auto min-h-screen bg-[#4A4042] border-x border-border/50">
+            {/* Header */}
+            <div className="sticky top-0 z-10 bg-[#4A4042]/95 backdrop-blur-md border-b border-border/50 px-4 py-3 flex items-center gap-4">
+                <button
+                    onClick={() => router.back()}
+                    className="p-2 hover:bg-white/10 rounded-full transition-colors text-white"
+                >
+                    <ArrowLeft className="w-5 h-5" />
+                </button>
+                <h1 className="font-bold text-lg text-white">Thread</h1>
+            </div>
+
+            <div className="p-4">
+                {/* Ancestors */}
+                <div className="mb-4">
+                    {ancestors.map(ancestor => (
+                        <div key={ancestor.id} className="relative">
+                            <CommentItem
+                                comment={ancestor}
+                                currentUserId={currentUserId}
+                                onCommentUpdated={refreshComments}
+                                isThreadView={true}
+                            />
+                            {/* Connector line to next item */}
+                            <div className="absolute left-[27px] top-[60px] bottom-[-16px] w-0.5 bg-border/50 -z-10" />
+                        </div>
+                    ))}
+                </div>
+
+                {/* Main Comment */}
+                <div className="relative">
+                    <div className="text-xl">
+                        <CommentItem
+                            comment={fullTargetComment}
+                            currentUserId={currentUserId}
+                            onCommentUpdated={refreshComments}
+                            isThreadView={true}
+                            isHero={true}
+                        />
+                    </div>
+                </div>
+
+                {/* Reply Input */}
+                <div className="py-4 border-b border-border/50">
+                    <CommentInput
+                        postId={fullTargetComment.post_id}
+                        parentId={fullTargetComment.id}
+                        onCommentAdded={refreshComments}
+                        placeholder={`Reply to @${fullTargetComment.profiles?.username}...`}
                     />
                 </div>
-                </div >
 
-            {/* Reply Input */ }
-            < div className = "py-4 border-b border-border/50" >
-                <CommentInput
-                    postId={fullTargetComment.post_id}
-                    parentId={fullTargetComment.id}
-                    onCommentAdded={refreshComments}
-                    placeholder={`Reply to @${fullTargetComment.profiles?.username}...`}
-                />
-                </div >
-
-            {/* Replies */ }
-            < div className = "mt-4" >
-            {
-                fullTargetComment.replies?.map(reply => (
-                    <CommentItem
-                        key={reply.id}
-                        comment={reply}
-                        currentUserId={currentUserId}
-                        onCommentUpdated={refreshComments}
-                        isThreadView={true}
-                    />
-                ))
-            }
-                </div >
-            </div >
-        </div >
+                {/* Replies */}
+                <div className="mt-4">
+                    {fullTargetComment.replies?.map(reply => (
+                        <CommentItem
+                            key={reply.id}
+                            comment={reply}
+                            currentUserId={currentUserId}
+                            onCommentUpdated={refreshComments}
+                            isThreadView={true}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
     );
-    }
+}
