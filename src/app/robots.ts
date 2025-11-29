e5369fbaa1f92b02e74b46773222442be0c2f76e
Mkobilan <matthew.kobilan@gmail.com>
2025-11-29 19:38:28 +0600

    updated url for sitemap and robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: ['/api/', '/settings/', '/profile/'],
         },
-        sitemap: 'https://hugloom.com/sitemap.xml',
+        sitemap: 'https://hugloom.vercel.app/sitemap.xml',
     }
 }
